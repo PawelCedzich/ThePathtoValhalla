b36f34f171205aeb5fea22121aebfdcba1f5251c
Perovsky56 <67289269+Perovsky56@users.noreply.github.com>
2023-03-26 17:32:49 +0200

    In-game menu fixes.
    Fixed "escape" key menu, added saving button, quiting to main menu and quiting game.

--- ThePathToValhalla01/Assets/Scripts/Menu.cs
+++ ThePathToValhalla01/Assets/Scripts/Menu.cs
@@ -7,20 +7,34 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         DataPersistenceManager.instance.NewGame();
+    }
+    public void ContinueGame()
+    {
+        Debug.Log("load");
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        DataPersistenceManager.instance.LoadGame();
+    }
+    public void QuitToMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(0);
     }
     public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+    public void SaveGame()
+    {
         DataPersistenceManager.instance.SaveGame();
+        Debug.Log("Game Saved Manually.");
     }
 
-    public void ContinueGame()
-    {
-        Debug.Log("load");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        DataPersistenceManager.instance.LoadGame();
-    }
 }
