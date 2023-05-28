68ff7add09aab03865cc82d337372aa45cf783c1
PawelCedzich <ced.paw.pc@gmail.com>
2023-05-28 20:54:26 +0200

    anvil timer

--- ThePathToValhalla01/Assets/Scripts/Interactions/Interactions/Anvil.cs
+++ ThePathToValhalla01/Assets/Scripts/Interactions/Interactions/Anvil.cs
@@ -7,22 +7,31 @@ public class Anvil : MonoBehaviour, IInteractable
     public GameObject player;
     private ItemPrefab item;
     public GameObject canvas;
+    public Interactor interactor;
+    public bool upgradedWeapon = false;
     private int timer;
     public void Interact()
     {
+        if (timer < 0)
+        {
+            upgradedWeapon = false;
             canvas.SetActive(true);
-            timer = 360;
+        }
+        else {
+            interactor.StopInteraction();
+        }
         
     }
 
     public void StopInteracting()
     {
         canvas.SetActive(false);
-        if (ChosenWeapon.GetCurrentSlot().Instance != null)
+        if (ChosenWeapon.GetCurrentSlot().Instance != null && upgradedWeapon == false)
         {
             item = ChosenWeapon.GetCurrentSlot().Instance.GetComponent<ItemPrefab>();
             item.Damage += 5;
-            Debug.Log(item.Damage);
+            timer = 3600;
+            upgradedWeapon = true;
         }
     }
 
