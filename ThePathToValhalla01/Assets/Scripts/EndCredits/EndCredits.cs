7a1b8254de3d801119848f344449e77a3704a83b
Perovsky56 <67289269+Perovsky56@users.noreply.github.com>
2023-05-30 20:54:46 +0200

    Fix Cursor after credits

--- ThePathToValhalla01/Assets/Scripts/EndCredits/EndCredits.cs
+++ ThePathToValhalla01/Assets/Scripts/EndCredits/EndCredits.cs
@@ -14,6 +14,7 @@ public class EndCredits : MonoBehaviour
     {
         StartCoroutine(countTime());
         yield return new WaitForSeconds(time);
+        Cursor.visible = true;
         SceneManager.LoadScene(0);
     }
 
