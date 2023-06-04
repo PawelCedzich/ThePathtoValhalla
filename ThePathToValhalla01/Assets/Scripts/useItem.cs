35e55e5c544408011e734ad852eaac11d2fd45c8
Perovsky56 <67289269+Perovsky56@users.noreply.github.com>
2023-06-04 18:45:31 +0200

    SoundEffect - eating

--- ThePathToValhalla01/Assets/Scripts/useItem.cs
+++ ThePathToValhalla01/Assets/Scripts/useItem.cs
@@ -10,24 +10,25 @@ public class useItem : MonoBehaviour
     public InventoryManager inventoryManager;
     public PlayerAttack playerAttack;
     public ThirdPersonController thirdPersonController;
+    public SoundPlayer soundPlayer;
     void Update()
     {
         if (Input.GetKeyDown("1") && inventoryManager.inventorySlots[2].Instance != null) {
             inventoryManager.inventorySlots[2].UseItem(playerStats, playerAttack, thirdPersonController);
             inventoryManager.inventorySlots[2].Instance.GetComponent<ItemPrefab>().SubtractItemAmount(1);
-
+            soundPlayer.PlaySound("eating", 1.0f, 0.3f);
         }
         if (Input.GetKeyDown("2") && inventoryManager.inventorySlots[3].Instance != null)
         {
             inventoryManager.inventorySlots[3].UseItem(playerStats, playerAttack, thirdPersonController);
             inventoryManager.inventorySlots[3].Instance.GetComponent<ItemPrefab>().SubtractItemAmount(1);
-
+            soundPlayer.PlaySound("eating", 1.0f, 0.3f);
         }
         if (Input.GetKeyDown("3") && inventoryManager.inventorySlots[4].Instance != null)
         {
             inventoryManager.inventorySlots[4].UseItem(playerStats, playerAttack, thirdPersonController);
             inventoryManager.inventorySlots[4].Instance.GetComponent<ItemPrefab>().SubtractItemAmount(1);
-
+            soundPlayer.PlaySound("eating", 1.0f, 0.3f);
         }
     }
 }
